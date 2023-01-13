804cd8014cc8150469a0ca7db4d614c915a4c079
Loriano Storchi <loriano@storchi.org>
2023-01-13 12:53:15 +0100

    trying different splittings

--- fittingviaGP_2D_CLI.py
+++ fittingviaGP_2D_CLI.py
@@ -25,6 +25,62 @@ if __name__ == "__main__":
         overalltrainmse = 0.0
         tot = 0
         traintot = 0
+        vib_values_torm = [[2, 4, 6, 8, 10, 14, 18, 22, 26, 30, 35],
+                [1, 3, 5, 7, 9, 12, 16, 20, 24, 28, 32, 40],
+                [2, 3, 5, 6, 8, 9, 12, 14, 18, 20, 24, 26, 30, 32],
+                [1, 2, 4, 5, 6, 7, 9, 10, 14, 16, 20, 22, 26, 28, 32, 35]]
+        for vrm in vib_values_torm:
+            print("Removing VIB ", vrm, flush=True)
+        
+            train_xy, train_z, test_xy, test_z = cm.get_train_and_test_rmv (temp_values, vib_values, \
+                df, vrm)
+        
+            model = cm.fitusingscikitl (train_xy, train_z)
+        
+            z_pred, std = model.predict(train_xy, return_std=True)
+            trainmse = 0.0
+            cont = 0.0
+            for i in range(train_z.shape[0]):
+                x = train_xy[i,0]
+                y = train_xy[i,1]
+                z = train_z[i]
+                zpred = z_pred[i]
+                zstd = std[i]
+                
+                trainmse += (zpred-z)**2
+                overalltrainmse += (zpred-z)**2
+                traintot += 1
+                cont += 1.0
+        
+                print("Train, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
+        
+            trainmse = trainmse/cont
+        
+            z_pred, std = model.predict(test_xy, return_std=True)
+            mse = 0.0
+            cont = 0.0
+            for i in range(test_z.shape[0]):
+                x = test_xy[i,0]
+                y = test_xy[i,1]
+                z = test_z[i]
+                zpred = z_pred[i]
+                zstd = std[i]
+                
+                mse += (zpred-z)**2
+                overallmse += (zpred-z)**2
+                tot += 1
+                cont += 1.0
+            
+                print("Test, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
+        
+            mse = mse/cont
+        
+            print("Removed VIB  , ", vrm, " ,", mse, " ,", trainmse, flush=True, file=ofp)
+    
+            print("Removed VIB  , ", vrm, " , MSE , ", mse, " , TrainMSE ,", trainmse, flush=True)
+        
+        print("Overall VIB MSE , ", overallmse/float(tot), \
+            " , Train MSE , ", overalltrainmse/float(traintot)
 
     else:
 
