558b84f1bb382db0a6521eed50d95e0cad8599ad
Loriano Storchi <loriano@storchi.org>
2023-01-27 14:19:46 +0100

    backup test using a function to compute also denorm MSE

--- fittingviaGP_2D_CLI.py
+++ fittingviaGP_2D_CLI.py
@@ -8,6 +8,86 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import commonmodules as cm
 
+#################################################################################################
+
+def get_mseresults (initialstring, ofp, model, train_xy, train_z, test_xy, test_z):
+
+    overallmse = 0.0 
+    denorm_overallmse = 0.0 
+    overalltrainmse = 0.0 
+    denorm_overalltrainmse = 0.0
+
+    tot = 0
+    traintot = 0
+
+    z_pred = model.predict(train_xy)
+    
+    trainmse = 0.0
+    denorm_trainmse = 0.0
+    cont = 0.0
+
+    for i in range(train_z.shape[0]):
+        x = train_xy[i,0]
+        y = train_xy[i,1]
+
+        z = train_z[i]
+        denorm_z = (z * (maxvalue - minvalue))+minvalue
+
+        zpred = z_pred[i]
+        denorm_zpred = (zpred * (maxvalue - minvalue))+minvalue
+        
+        trainmse += (zpred-z)**2
+        denorm_trainmse += (denorm_zpred-denorm_z)**2
+
+        overalltrainmse += (zpred-z)**2
+        denorm_overalltrainmse += (denorm_zpred-denorm_z)**2
+
+        traintot += 1
+        cont += 1.0
+    
+        print("Train, %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred))
+    
+    trainmse = trainmse/cont
+    denorm_trainmse = denorm_trainmse/cont
+    
+    z_pred = model.predict(test_xy)
+    mse = 0.0
+    denorm_mse = 0.0
+    cont = 0.0
+    for i in range(test_z.shape[0]):
+        x = test_xy[i,0]
+        y = test_xy[i,1]
+
+        z = test_z[i]
+        denorm_z = (z * (maxvalue - minvalue))+minvalue
+
+        zpred = z_pred[i]
+        denorm_zpred = (zpred * (maxvalue - minvalue))+minvalue
+
+        mse += (zpred-z)**2
+        denorm_mse += (denorm_zpred-denorm_z)**2
+
+        overallmse += (zpred-z)**2
+        tot += 1
+        cont += 1.0
+    
+        print("Test, %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred))
+    
+    mse = mse/cont
+    denorm_mse = denorm_mse/cont
+    
+    print(initialstring, " ,", mse, " ,", trainmse, " ,", denorm_mse , \
+        " ,",  denorm_trainmse, flush=True, file=ofp)
+    
+    print(initialstring, " , MSE , ", mse, " , TrainMSE ,", trainmse, \
+        " , Denorm. MSE , ", denorm_mse, " , Denorm. TrainMSE ,", \
+            denorm_trainmse, flush=True)
+
+    return overallmse, denorm_overallmse, overalltrainmse, \
+        denorm_overalltrainmse, tot, traintot
+
+#################################################################################################
+
 if __name__ == "__main__":
 
     filename = "testdv1.xlsx"
@@ -28,6 +108,9 @@ if __name__ == "__main__":
     
         overallmse = 0.0
         overalltrainmse = 0.0
+        denorm_overalltrainmse = 0.0
+        denorm_overallmse = 0.0
+
         tot = 0
         traintot = 0
 
@@ -60,53 +143,27 @@ if __name__ == "__main__":
             elif modelname == "model2":
                 model = cm.build_model_GP_2 (train_xy, train_z)
 
-            z_pred, std = model.predict(train_xy, return_std=True)
-            trainmse = 0.0
-            cont = 0.0
-            for i in range(train_z.shape[0]):
-                x = train_xy[i,0]
-                y = train_xy[i,1]
-                z = train_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-                
-                trainmse += (zpred-z)**2
-                overalltrainmse += (zpred-z)**2
-                traintot += 1
-                cont += 1.0
-        
-                print("Train, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            trainmse = trainmse/cont
-        
-            z_pred, std = model.predict(test_xy, return_std=True)
-            mse = 0.0
-            cont = 0.0
-            for i in range(test_z.shape[0]):
-                x = test_xy[i,0]
-                y = test_xy[i,1]
-                z = test_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-                
-                mse += (zpred-z)**2
-                overallmse += (zpred-z)**2
-                tot += 1
-                cont += 1.0
-            
-                print("Test, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            mse = mse/cont
-        
-            print("Removed VIB  , ", str(vrm).replace(",", ";"), " ,", mse, " ,", trainmse, flush=True, file=ofp)
-        
-            print("Removed VIB  , ", vrm, " , MSE , ", mse, " , TrainMSE ,", trainmse, flush=True)
-        
+            initialstring = "Removed VIB  , " + str(vrm).replace(",", ";")
+            l_overallmse, l_denorm_overallmse, \
+                l_overalltrainmse, l_denorm_overalltrainmse, \
+                    l_tot,  l_traintot = get_mseresults (initialstring, ofp, model, \
+                    train_xy, train_z, test_xy, test_z)
+
+            overallmse += l_overallmse
+            overalltrainmse += l_overalltrainmse
+            tot += l_tot
+            traintot += l_traintot            
+
         print("Overall VIB MSE , ", overallmse/float(tot), \
-            " , Train MSE , ", overalltrainmse/float(traintot))
+            ", Train MSE , ", overalltrainmse/float(traintot), \
+            ", Denorm. MSE , ", denorm_overallmse/float(tot), \
+            ", Denorm. Train MSE , ", denorm_overalltrainmse/float(traintot))
     
         overallmse = 0.0
         overalltrainmse = 0.0
+        denorm_overalltrainmse = 0.0
+        denorm_overallmse = 0.0
+
         tot = 0
         traintot = 0
         for trm in temp_values:
@@ -122,53 +179,26 @@ if __name__ == "__main__":
             elif modelname == "model2":
                 model = cm.build_model_GP_2 (train_xy, train_z)
 
-            z_pred, std = model.predict(train_xy, return_std=True)
-            trainmse = 0.0
-            cont = 0.0
-            for i in range(train_z.shape[0]):
-                x = train_xy[i,0]
-                y = train_xy[i,1]
-                z = train_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-                
-                trainmse += (zpred-z)**2
-                overalltrainmse += (zpred-z)**2
-                traintot += 1
-                cont += 1.0
-        
-                print("Train, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            trainmse = trainmse/cont
-        
-            z_pred, std = model.predict(test_xy, return_std=True)
-            mse = 0.0
-            cont = 0.0
-            for i in range(test_z.shape[0]):
-                x = test_xy[i,0]
-                y = test_xy[i,1]
-                z = test_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-        
-                mse += (zpred-z)**2
-                overallmse += (zpred-z)**2
-                tot += 1
-                cont += 1.0
-        
-                print("Test, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            mse = mse/cont
-        
-            print("Removed TEMP , ", trm, " ,", mse, " ,", trainmse, flush=True, file=ofp)
-        
-            print("Removed TEMP , ", trm, " , MSE , ", mse, " , TrainMSE ,", trainmse, flush=True)
-        
+            initialstring = "Removed TEMP  , " + str(trm)
+            l_overallmse, l_denorm_overallmse, \
+                l_overalltrainmse, l_denorm_overalltrainmse, \
+                    l_tot,  l_traintot = get_mseresults (initialstring, ofp, model, \
+                    train_xy, train_z, test_xy, test_z)
+
+            overallmse += l_overallmse
+            overalltrainmse += l_overalltrainmse
+            tot += l_tot
+            traintot += l_traintot
+
         print("Overall TEMP MSE , ", overallmse/float(tot), \
-            " , Train MSE , ", overalltrainmse/float(traintot))
+            ", Train MSE , ", overalltrainmse/float(traintot), \
+            ", Denorm. MSE , ", denorm_overallmse/float(tot), \
+            ", Denorm. Train MSE , ", denorm_overalltrainmse/float(traintot))                
         
         overallmse = 0.0
         overalltrainmse = 0.0
+        denorm_overalltrainmse = 0.0
+        denorm_overallmse = 0.0
         tot = 0
         traintot = 0
         for vrm in vib_values:
@@ -183,51 +213,22 @@ if __name__ == "__main__":
                 model = cm.build_model_GP_1 (train_xy, train_z)
             elif modelname == "model2":
                 model = cm.build_model_GP_2 (train_xy, train_z)
-                
-            z_pred, std = model.predict(train_xy, return_std=True)
-            trainmse = 0.0
-            cont = 0.0
-            for i in range(train_z.shape[0]):
-                x = train_xy[i,0]
-                y = train_xy[i,1]
-                z = train_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-                
-                trainmse += (zpred-z)**2
-                overalltrainmse += (zpred-z)**2
-                traintot += 1
-                cont += 1.0
-        
-                print("Train, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            trainmse = trainmse/cont
-        
-            z_pred, std = model.predict(test_xy, return_std=True)
-            mse = 0.0
-            cont = 0.0
-            for i in range(test_z.shape[0]):
-                x = test_xy[i,0]
-                y = test_xy[i,1]
-                z = test_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-                
-                mse += (zpred-z)**2
-                overallmse += (zpred-z)**2
-                tot += 1
-                cont += 1.0
-            
-                print("Test, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            mse = mse/cont
-        
-            print("Removed VIB  , ", vrm, " ,", mse, " ,", trainmse, flush=True, file=ofp)
-        
-            print("Removed VIB  , ", vrm, " , MSE , ", mse, " , TrainMSE ,", trainmse, flush=True)
-        
+
+            initialstring = "Removed VIB  , " + str(vrm)
+            l_overallmse, l_denorm_overallmse, \
+                l_overalltrainmse, l_denorm_overalltrainmse, \
+                    l_tot,  l_traintot = get_mseresults (initialstring, ofp, model, \
+                    train_xy, train_z, test_xy, test_z)
+
+            overallmse += l_overallmse
+            overalltrainmse += l_overalltrainmse
+            tot += l_tot
+            traintot += l_traintot
+
         print("Overall VIB MSE , ", overallmse/float(tot), \
-            " , Train MSE , ", overalltrainmse/float(traintot))
+            ", Train MSE , ", overalltrainmse/float(traintot), \
+            ", Denorm. MSE , ", denorm_overallmse/float(tot), \
+            ", Denorm. Train MSE , ", denorm_overalltrainmse/float(traintot))                
         
         for perc in [0.05, 0.10, 0.20, 0.30, 0.40, 0.50]:
         
@@ -239,43 +240,9 @@ if __name__ == "__main__":
                 model = cm.build_model_GP_1 (train_xy, train_z)
             elif modelname == "model2":
                 model = cm.build_model_GP_2 (train_xy, train_z)
-                        
-            z_pred, std = model.predict(train_xy, return_std=True)
-            trainmse = 0.0
-            cont = 0.0
-            for i in range(train_z.shape[0]):
-                x = train_xy[i,0]
-                y = train_xy[i,1]
-                z = train_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-            
-                trainmse += (zpred-z)**2
-                cont += 1.0
-        
-                print("Train, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            trainmse = trainmse/cont
-        
-            z_pred, std = model.predict(test_xy, return_std=True)
-            mse = 0.0
-            cont = 0.0
-            for i in range(test_z.shape[0]):
-                x = test_xy[i,0]
-                y = test_xy[i,1]
-                z = test_z[i]
-                zpred = z_pred[i]
-                zstd = std[i]
-            
-                mse += (zpred-z)**2
-                cont += 1.0
-                
-                print("Test, %10.7f , %10.7f , %10.7f , %10.7f , %10.7f"%(z, y, z, zpred, zstd))
-        
-            mse = mse/cont
-        
-            print("Removed RND  , ", perc, " ,", mse, " ,", trainmse, flush=True, file=ofp)
-        
-            print("Removed random values ", perc ,"  MSE ", mse, " , TrainMSE ,", \
-                trainmse, flush=True) 
 
+            initialstring = "Removed RND  , " + str(perc)
+            l_overallmse, l_denorm_overallmse, \
+                l_overalltrainmse, l_denorm_overalltrainmse, \
+                    l_tot,  l_traintot = get_mseresults (initialstring, ofp, model, \
+                    train_xy, train_z, test_xy, test_z)
