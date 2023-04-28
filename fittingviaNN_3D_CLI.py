1833b18b986f44646be5d9fdfc36f36dde168fc6
Loriano Storchi <loriano@storchi.org>
2023-04-28 14:44:34 +0200

    basic code for the gridsearch ready

--- fittingviaNN_3D_CLI.py
+++ fittingviaNN_3D_CLI.py
@@ -21,7 +21,8 @@ import commonmodules as cm
 
 #######################################################################
 
-def build_perc_split (modelshape, batch_size, epochs, modelfname, verbose=False):
+def build_perc_split (modelshape, batch_size, epochs, \
+                      modelfname="", verbose=False):
 
     ofp = None 
     if modelfname != "":
@@ -32,17 +33,17 @@ def build_perc_split (modelshape, batch_size, epochs, modelfname, verbose=False)
     avgmse_test = 0.0
     avgmse_train = 0.0
 
+    num = 0.0
+
     if verbose:
         print (" Perc. Split , Test MSE , Test R2 , Train MSE , Train R2")
     if modelfname != "":
         print (" Perc. Split , Test MSE , Test R2 , Train MSE , Train R2", file=ofp)
 
-    num = 1.0
     for perc in [0.05, 0.10, 0.25, 0.30, 0.50]:
         train_x, test_x, train_y, test_y = train_test_split(x_s, y_s, \
                     test_size=perc, random_state=42)
         
-        num += 1.0
 
         modelshape = [32, 32, 32, 32]
         epochs = 10
@@ -66,12 +67,205 @@ def build_perc_split (modelshape, batch_size, epochs, modelfname, verbose=False)
         avgr2_train += trainr2
         avgmse_train += trainmse
         
+        num += 1.0
+
         if verbose:
             print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(perc, testmse, testr2, \
                                                        trainmse,  trainr2))
         if modelfname != "":
             print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(perc, testmse, testr2, \
                                                        trainmse,  trainr2), file=ofp)
+            
+    if modelfname != "":
+        ofp.close()
+
+    return avgr2_train/num, avgmse_train/num, avgr2_test/num, avgmse_test/num
+
+#######################################################################
+
+def build_v_split (modelshape, batch_size, epochs, \
+                   modelfname="", verbose=False):
+
+    ofp = None
+    if modelfname != "":
+        ofp = open(modelfname, "w")
+
+    avgr2_test = 0.0
+    avgr2_train = 0.0
+    avgmse_test = 0.0
+    avgmse_train = 0.0
+
+    num = 0.0
+
+    thefirst = True
+    if verbose:
+        print (" v Removed , Test MSE , Test R2 , Train MSE , Train R2")
+    if modelfname != "":
+        print (" v Removed , Test MSE , Test R2 , Train MSE , Train R2", file=ofp)
+    for v in vset:
+        train_x, test_x, train_y, test_y = cm.test_train_split (0, [v], x_s, y_s)
+
+        if thefirst:
+            model = cm.buildmodel(modelshape)
+            history = model.fit(train_x, train_y, epochs=epochs,  batch_size=batch_size, \
+                verbose=0)
+            thefirst = False
+
+        model = cm.buildmodel(modelshape)
+        history = model.fit(train_x, train_y, epochs=epochs,  batch_size=batch_size, \
+            verbose=0)
+
+        pred_y = model.predict(test_x, verbose=0)
+        testmse = metrics.mean_absolute_error(test_y, pred_y)
+        testr2 = metrics.r2_score(test_y, pred_y)
+
+        avgr2_test += testr2
+        avgmse_test += testmse
+
+        pred_y = model.predict(train_x, verbose=0)
+        trainmse = metrics.mean_absolute_error(train_y, pred_y)
+        trainr2 = metrics.r2_score(train_y, pred_y)
+
+        avgr2_train += trainr2
+        avgmse_train += trainmse
+
+        num += 1.0
+        
+        if verbose:
+            print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(v, testmse, testr2, \
+                                                        trainmse,  trainr2))
+        
+        if modelfname != "":
+            print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(v, testmse, testr2, \
+                                                        trainmse,  trainr2), file=ofp)
+    
+    if modelfname != "":
+        ofp.close()
+
+    return avgr2_train/num, avgmse_train/num, avgr2_test/num, avgmse_test/num
+
+#######################################################################
+
+def build_w_split (modelshape, batch_size, epochs, \
+                   modelfname="", verbose=False):
+    ofp = None
+    if modelfname != "":
+        ofp = open("wremoved.csv", "w")
+
+    thefirst = True
+
+    avgr2_test = 0.0
+    avgr2_train = 0.0
+    avgmse_test = 0.0
+    avgmse_train = 0.0
+
+    num = 0.0
+
+    if verbose:
+        print (" w Removed , Test MSE , Test R2 , Train MSE , Train R2")
+    if modelfname != "":
+        print (" w Removed , Test MSE , Test R2 , Train MSE , Train R2", file=ofp)
+
+    for w in wset:
+        train_x, test_x, train_y, test_y = cm.test_train_split (1, [w], x_s, y_s)
+
+        if thefirst:
+            model = cm.buildmodel(modelshape)
+            history = model.fit(train_x, train_y, epochs=epochs,  batch_size=batch_size, \
+                verbose=0)
+            thefirst = False
+
+        model = cm.buildmodel(modelshape)
+        history = model.fit(train_x, train_y, epochs=epochs,  batch_size=batch_size, \
+            verbose=0)
+
+        pred_y = model.predict(test_x, verbose=0)
+        testmse = metrics.mean_absolute_error(test_y, pred_y)
+        testr2 = metrics.r2_score(test_y, pred_y)
+
+        avgr2_test += testr2
+        avgmse_test += testmse
+
+        pred_y = model.predict(train_x, verbose=0)
+        trainmse = metrics.mean_absolute_error(train_y, pred_y)
+        trainr2 = metrics.r2_score(train_y, pred_y)
+
+        avgr2_train += trainr2
+        avgmse_train += trainmse
+
+        num += 1.0
+
+        if verbose:
+            print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(w, testmse, testr2, \
+                                                        trainmse,  trainr2))
+        if modelfname != "":
+            print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(w, testmse, testr2, \
+                                                        trainmse,  trainr2), file=ofp)
+    if modelfname != "":  
+        ofp.close()
+
+    return avgr2_train/num, avgmse_train/num, avgr2_test/num, avgmse_test/num
+
+#######################################################################
+
+def build_t_split (modelshape, batch_size, epochs, \
+                   modelfname="", verbose=False):
+    ofp = None
+    if modelfname != "":
+        ofp = open("tremoved.csv", "w")
+
+    thefirst = True
+
+    avgr2_test = 0.0
+    avgr2_train = 0.0
+    avgmse_test = 0.0
+    avgmse_train = 0.0
+
+    num = 0.0
+
+    if verbose:
+        print (" T Removed , Test MSE , Test R2 , Train MSE , Train R2")
+    if modelfname != "":
+        print (" T Removed , Test MSE , Test R2 , Train MSE , Train R2", file=ofp)
+    for t in tset:
+        train_x, test_x, train_y, test_y = cm.test_train_split (2, [t], x_s, y_s)
+
+        modelshape = [32, 32, 32, 32]
+        epochs = 10
+        batch_size = 50
+
+        if thefirst:
+            model = cm.buildmodel(modelshape)
+            history = model.fit(train_x, train_y, epochs=epochs,  batch_size=batch_size, \
+                verbose=0)
+            thefirst = False
+
+        model = cm.buildmodel(modelshape)
+        history = model.fit(train_x, train_y, epochs=epochs,  batch_size=batch_size, \
+            verbose=0)
+
+        pred_y = model.predict(test_x, verbose=0)
+        testmse = metrics.mean_absolute_error(test_y, pred_y)
+        testr2 = metrics.r2_score(test_y, pred_y)
+        
+        avgr2_test += testr2
+        avgmse_test += testmse
+    
+        pred_y = model.predict(train_x, verbose=0)
+        trainmse = metrics.mean_absolute_error(train_y, pred_y)
+        trainr2 = metrics.r2_score(train_y, pred_y)
+
+        avgr2_train += trainr2
+        avgmse_train += trainmse
+
+        num += 1.0
+
+        if verbose:
+            print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(t, testmse, testr2, \
+                                                        trainmse,  trainr2))
+        if modelfname != "":
+            print("%5.2f , %10.6f , %10.6f , %10.6f , %10.6f"%(t, testmse, testr2, \
+                                                        trainmse,  trainr2), file=ofp)
             
     if modelfname != "":
         ofp.close()
@@ -132,12 +326,70 @@ if __name__ == "__main__":
     epochs = 10
     batch_size_s = [50]
 
+    modelnum = 0
+
     for modelshape in modelshape_s:
         for batch_size in batch_size_s:
-            modelfname = "perc.csv"
+            #modelfname = "perc.csv"
+
+            r2test = 0.0
+            msetest = 0.0
+            r2train = 0.0 
+            msetrain = 0.0
+
             avgr2_train, avgmse_train, avgr2_test, avgmse_test = \
-                build_perc_split (modelshape, batch_size, epochs, modelfname)
+                build_perc_split (modelshape, batch_size, epochs)
             
-            print("%10.5f %10.5f %10.5f %10.5f"%(avgmse_train, avgr2_train, \
+            r2test += avgr2_test
+            msetest += avgmse_test
+            r2train += avgr2_train
+            msetrain += avgmse_train
+            
+            print("  Perc, %10.5f , %10.5f , %10.5f , %10.5f"%(avgmse_train, avgr2_train, \
                                                   avgmse_test,  avgr2_test))
             
+            avgr2_train, avgmse_train, avgr2_test, avgmse_test = \
+                build_v_split (modelshape, batch_size, epochs)
+            
+            r2test += avgr2_test
+            msetest += avgmse_test
+            r2train += avgr2_train
+            msetrain += avgmse_train
+            
+            print("vSplit, %10.5f , %10.5f , %10.5f , %10.5f"%(avgmse_train, avgr2_train, \
+                                                  avgmse_test,  avgr2_test))
+            
+            avgr2_train, avgmse_train, avgr2_test, avgmse_test = \
+                build_w_split (modelshape, batch_size, epochs)
+            
+            r2test += avgr2_test
+            msetest += avgmse_test
+            r2train += avgr2_train
+            msetrain += avgmse_train
+            
+            print("wSplit, %10.5f , %10.5f , %10.5f , %10.5f"%(avgmse_train, avgr2_train, \
+                                                  avgmse_test,  avgr2_test))
+            
+            avgr2_train, avgmse_train, avgr2_test, avgmse_test = \
+                build_t_split (modelshape, batch_size, epochs)
+            
+            r2test += avgr2_test
+            msetest += avgmse_test
+            r2train += avgr2_train
+            msetrain += avgmse_train
+            
+            print("TSplit, %10.5f , %10.5f , %10.5f , %10.5f"%(avgmse_train, avgr2_train, \
+                                                  avgmse_test,  avgr2_test))
+            
+            modelnum += 1
+            r2test = r2test/4.0
+            msetest = msetest/4.0
+            r2train = r2train/4.0
+            msetrain = msetrain/4.0
+
+            print("Model metrics %3d , %10.5f , %10.5f , %10.5f , %10.5f"%( \
+                 modelnum, r2test, msetest, r2train, msetrain))
+            print("Model shapes  %3d , %s , %5d "%( \
+                 modelnum, str(modelshape), batch_size))
+
+
