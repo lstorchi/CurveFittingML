2f921dca49642d0780ab1faebc819bb98e8c9c67
Loriano Storchi <loriano@storchi.org>
2025-02-19 15:44:02 +0100

    all v

--- fittingviaGP_3D.py
+++ fittingviaGP_3D.py
@@ -46,9 +46,8 @@ if __name__ == "__main__":
             print(ys, y[i])
         for i, xs in enumerate(x_s):
             print(xs, x[i])
-    
-   
-    for nu in [1.0/2.0, 1.0, 2.0, 5.0/2.0, 3.0]:
+     
+    for nu in [2.0]:
         ofp = open("vremoved_GP_"+str(nu)+".csv", "w")
     
         avgr2test = 0.0
@@ -60,63 +59,63 @@ if __name__ == "__main__":
 
         starttime = time.time()
         for v in vset:
-            if vmap_toreal[v] == 40:
-                train_x, test_x, train_y, test_y = cm.test_train_split (0, [v], x_s, y_s)
             
-                model = cm.build_model_GP_3D (train_x, train_y, nuval=nu)
-                
-                ofptest = open("vremoved_GP_"+\
-                               str(nu)+"_"+ \
-                               str(vmap_toreal[v])+"_test.csv", "w")
-                print (" v , w , T , y , y_pred ", file=ofptest)
-                print ("Test Shape : ", test_x.shape)
-                test_x_sp = scalerx.inverse_transform(test_x)
-                pred_y = model.predict(test_x)
-                print ("Pred y Shape ", pred_y.shape)
-                print ("Test y Shape ", test_y.shape)
-                pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1,1))
-                test_y_sb = scalery.inverse_transform(test_y.reshape(-1,1))
-                for i, yt in enumerate(test_y_sb):
-                     print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(test_x_sp[i,0], 
-                                                         test_x_sp[i,1],
-                                                         test_x_sp[i,2],
-                                                         yt,
-                                                         pred_y_sb[i]), file=ofptest, flush=True)
-                #plt.scatter(test_y_sb, pred_y_sb)
-                #plt.show()
-                testmse = metrics.mean_absolute_error(test_y_sb, pred_y_sb)
-                testr2 = metrics.r2_score(test_y_sb, pred_y_sb)
-                ofptest.close()
+            train_x, test_x, train_y, test_y = cm.test_train_split (0, [v], x_s, y_s)
             
-                ofptrain = open("vremoved_GP_"+str(vmap_toreal[v])+"_train.csv", "w")
-                print (" v , w , T , y , y_pred  ", file=ofptrain)
-                pred_y = model.predict(train_x)
-                print ("Train y Shape ", train_y.shape)
-                print ("Pred y Shape ", pred_y.shape)
-                pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1,1))
-                train_y_sb = scalery.inverse_transform(train_y.reshape(-1,1))
-                train_x_sp = scalerx.inverse_transform(train_x)
-                for i, yt in enumerate(train_y_sb):
-                     print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(train_x_sp[i,0], 
-                                                         train_x_sp[i,1],
-                                                         train_x_sp[i,2],
-                                                         yt,
-                                                         pred_y_sb[i]), file=ofptrain, flush=True)
-                #plt.scatter(train_y_sb, pred_y_sb)
-                #plt.show()
-                trainmse = metrics.mean_absolute_error(train_y_sb, pred_y_sb)
-                trainr2 = metrics.r2_score(train_y_sb, pred_y_sb)
-                ofptrain.close()
-                
-                #print("%3d , %10.6e , %10.6f , %10.6e , %10.6f"%(vmap_toreal[v], testmse, testr2, \
-                #                                                   trainmse,  trainr2))
-                
-                print("%3d , %10.6e , %10.6f , %10.6e , %10.6f"%(vmap_toreal[v], testmse, testr2, \
-                                                                   trainmse,  trainr2), file=ofp)
-                avgmsetest += testmse
-                avgr2test += testr2
-                avgmsetrain += trainmse
-                avgr2train += trainr2
+            model = cm.build_model_GP_3D (train_x, train_y, nuval=nu)
+            
+            ofptest = open("vremoved_GP_"+\
+                           str(nu)+"_"+ \
+                           str(vmap_toreal[v])+"_test.csv", "w")
+            print (" v , w , T , y , y_pred ", file=ofptest)
+            print ("Test Shape : ", test_x.shape)
+            test_x_sp = scalerx.inverse_transform(test_x)
+            pred_y = model.predict(test_x)
+            print ("Pred y Shape ", pred_y.shape)
+            print ("Test y Shape ", test_y.shape)
+            pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1,1))
+            test_y_sb = scalery.inverse_transform(test_y.reshape(-1,1))
+            for i, yt in enumerate(test_y_sb):
+                 print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(test_x_sp[i,0], 
+                                                     test_x_sp[i,1],
+                                                     test_x_sp[i,2],
+                                                     yt,
+                                                     pred_y_sb[i]), file=ofptest, flush=True)
+            #plt.scatter(test_y_sb, pred_y_sb)
+            #plt.show()
+            testmse = metrics.mean_absolute_error(test_y_sb, pred_y_sb)
+            testr2 = metrics.r2_score(test_y_sb, pred_y_sb)
+            ofptest.close()
+            
+            ofptrain = open("vremoved_GP_"+str(vmap_toreal[v])+"_train.csv", "w")
+            print (" v , w , T , y , y_pred  ", file=ofptrain)
+            pred_y = model.predict(train_x)
+            print ("Train y Shape ", train_y.shape)
+            print ("Pred y Shape ", pred_y.shape)
+            pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1,1))
+            train_y_sb = scalery.inverse_transform(train_y.reshape(-1,1))
+            train_x_sp = scalerx.inverse_transform(train_x)
+            for i, yt in enumerate(train_y_sb):
+                 print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(train_x_sp[i,0], 
+                                                     train_x_sp[i,1],
+                                                     train_x_sp[i,2],
+                                                     yt,
+                                                     pred_y_sb[i]), file=ofptrain, flush=True)
+            #plt.scatter(train_y_sb, pred_y_sb)
+            #plt.show()
+            trainmse = metrics.mean_absolute_error(train_y_sb, pred_y_sb)
+            trainr2 = metrics.r2_score(train_y_sb, pred_y_sb)
+            ofptrain.close()
+            
+            #print("%3d , %10.6e , %10.6f , %10.6e , %10.6f"%(vmap_toreal[v], testmse, testr2, \
+            #                                                   trainmse,  trainr2))
+            
+            print("%3d , %10.6e , %10.6f , %10.6e , %10.6f"%(vmap_toreal[v], testmse, testr2, \
+                                                               trainmse,  trainr2), file=ofp)
+            avgmsetest += testmse
+            avgr2test += testr2
+            avgmsetrain += trainmse
+            avgr2train += trainr2
         ofp.close()
         endtime = time.time()
         print("Time taken for nu = %4.2f is %10.6f"%(nu, endtime-starttime))
