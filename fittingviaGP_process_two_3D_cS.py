f02e20c4dafa6d6d6b959520b72358a12ba7b63b
Loriano Storchi <loriano@storchi.org>
2025-03-21 18:28:22 +0100

    backup

--- fittingviaGP_process_two_3D_cS.py
+++ fittingviaGP_process_two_3D_cS.py
@@ -70,51 +70,34 @@ if __name__ == "__main__":
             train_x, test_x, train_y, test_y = cm.test_train_split (0, [v], x_s, y_s)
             
             model = cm.build_model_GP_3D (train_x, train_y, nuval=nu)
-            
-            ofptest = open("vremoved_GP_"+\
-                           str(nu)+"_"+ \
-                           str(vmap_toreal[v])+"_test.csv", "w")
-            print (" v , dE , cE , y , y_pred ", file=ofptest)
-            print ("Test Shape : ", test_x.shape)
-            test_x_sb = scalerx.inverse_transform(test_x)
+
             pred_y = model.predict(test_x)
-            print ("Pred y Shape ", pred_y.shape)
-            print ("Test y Shape ", test_y.shape)
+            test_x_sb = scalerx.inverse_transform(test_x)
             pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1,1))
             test_y_sb = scalery.inverse_transform(test_y.reshape(-1,1))
-            for i, yt in enumerate(test_y_sb):
-                 print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(test_x_sb[i,0], 
-                                                     test_x_sb[i,1],
-                                                     test_x_sb[i,2],
-                                                     yt,
-                                                     pred_y_sb[i]), file=ofptest, flush=True)
-            #plt.scatter(test_y_sb, pred_y_sb)
-            #plt.show()
+            with open("vremoved_GP_"+ str(nu)+"_"+ \
+                        str(vmap_toreal[v])+"_test.csv", "w") as ofptest:
+                for ix, xval in enumerate(test_x_sb):
+                    for xx in xval:
+                        print("%10.8e , "%xx, end="", file=ofptest)
+                    print("%10.8e , %10.8e"%(pred_y_sb[ix][0], \
+                                             test_y_sb[ix][0]), file=ofptest)
             testmse = metrics.mean_absolute_error(test_y_sb, pred_y_sb)
             testr2 = metrics.r2_score(test_y_sb, pred_y_sb)
-            ofptest.close()
             
-            ofptrain = open("vremoved_GP_"+\
-                str(nu)+"_"+ \
-                str(vmap_toreal[v])+"_train.csv", "w")
-            print (" v , dE , cE , y , y_pred  ", file=ofptrain)
             pred_y = model.predict(train_x)
-            #print ("Train y Shape ", train_y.shape)
-            #print ("Pred y Shape ", pred_y.shape)
             pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1,1))
             train_y_sb = scalery.inverse_transform(train_y.reshape(-1,1))
             train_x_sb = scalerx.inverse_transform(train_x)
-            for i, yt in enumerate(train_y_sb):
-                 print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(train_x_sb[i,0], 
-                                                     train_x_sb[i,1],
-                                                     train_x_sb[i,2],
-                                                     yt,
-                                                     pred_y_sb[i]), file=ofptrain, flush=True)
-            #plt.scatter(train_y_sb, pred_y_sb)
-            #plt.show()
+            with open("vremoved_GP_"+ str(nu)+"_"+ \
+                        str(vmap_toreal[v])+"_train.csv", "w") as ofptrain:
+                for ix, xval in enumerate(train_x_sb):
+                    for xx in xval:
+                        print("%10.8e , "%xx, end="", file=ofptrain)
+                    print("%10.8e , %10.8e"%(pred_y_sb[ix][0], \
+                                             train_y_sb[ix][0]), file=ofptrain)
             trainmse = metrics.mean_absolute_error(train_y_sb, pred_y_sb)
             trainr2 = metrics.r2_score(train_y_sb, pred_y_sb)
-            ofptrain.close()
             
             print("%3d , %10.6e , %10.6f , %10.6e , %10.6f"%(vmap_toreal[v], testmse, testr2, \
                                                                trainmse,  trainr2))
@@ -203,41 +186,34 @@ if __name__ == "__main__":
         
             ofptest = open("vsetremoved_GP_set"+str(setid+1)+\
                 "_" + str(nu) + "_test.csv", "w")
-            print (" v , dE , cE , y , y_pred", file=ofptest, flush=True)
             pred_y = model.predict(test_x)
             test_x_sb = scalerx.inverse_transform(test_x)
             pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1, 1))
             test_y_sb = scalery.inverse_transform(test_y.reshape(-1, 1))
-            for i, yt in enumerate(test_y_sb):
-                print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(test_x_sb[i,0], 
-                                                     test_x_sb[i,1],
-                                                     test_x_sb[i,2],
-                                                     yt,
-                                                     pred_y_sb[i]), file=ofptest, flush=True)
-            #plt.scatter(test_y_sb, pred_y_sb)
-            #plt.show()
+            with open("vsetremoved_GP_set"+str(setid+1)+\
+                        "_" + str(nu) + "_test.csv", "w") as ofptest:
+                for ix, xval in enumerate(test_x_sb):
+                    for xx in xval:
+                        print("%10.8e , "%xx, end="", file=ofptest)
+                    print("%10.8e , %10.8e"%(pred_y_sb[ix][0], \
+                                                test_y_sb[ix][0]), file=ofptest)
+
             testmse = metrics.mean_absolute_error(test_y_sb, pred_y_sb)
             testr2 = metrics.r2_score(test_y_sb, pred_y_sb)
-            ofptest.close()
         
-            ofptrain = open("vsetremoved_GP_set"+str(setid+1)+\
-                "_" + str(nu) + "_train.csv", "w")
-            print (" v , dE , cE , y , y_pred  ", file=ofptrain)
             pred_y = model.predict(train_x)
             pred_y_sb = scalery.inverse_transform(pred_y.reshape(-1, 1))
             train_y_sb = scalery.inverse_transform(train_y.reshape(-1, 1))
             train_x_sb = scalerx.inverse_transform(train_x)
-            for i, yt in enumerate(train_y_sb):
-                 print (" %3d , %3d , %6d , %10.8e , %10.8e  "%(train_x_sb[i,0], 
-                                                     train_x_sb[i,1],
-                                                     train_x_sb[i,2],
-                                                     yt,
-                                                     pred_y_sb[i]), file=ofptrain, flush=True)
-            #plt.scatter(train_y_sb, pred_y_sb)
-            #plt.show()
+            with open("vsetremoved_GP_set"+str(setid+1)+\
+                        "_" + str(nu) + "_train.csv", "w") as ofptrain:
+                for i, xval in enumerate(train_x_sb):
+                    for xx in xval:
+                        print("%10.8e , "%xx, end="", file=ofptrain)
+                    print("%10.8e , %10.8e"%(pred_y_sb[i][0], \
+                                                train_y_sb[i][0]), file=ofptrain)
             trainmse = metrics.mean_absolute_error(train_y_sb, pred_y_sb)
             trainr2 = metrics.r2_score(train_y_sb, pred_y_sb)
-            ofptrain.close()
                 
             print("%s , %10.6e , %10.6f , %10.6e , %10.6f"%(str(v_sp).replace(",",";"), testmse, testr2, \
                                                                 trainmse,  trainr2), flush=True)
